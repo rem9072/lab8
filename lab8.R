013ae20a410c4aab4f7837ec2d50995fd3a5627a
reed magleby <rem9072@med.cornell.edu>
2018-11-06 09:45:00 -0500

    first commit

---
+++ lab8.R
@@ -0,0 +1,48 @@
+#Question 1
+
+prime <- function(n) {
+  if (n %% 1 != 0 | n < 0) {
+    return(FALSE)
+  } else if (n == 1) {
+    return(FALSE)
+  } else if (n == 2) {
+    return(TRUE)
+  } else {
+    for (i in 2:(sqrt(n))) {
+      if (n %% i == 0) return(FALSE) else return(TRUE)
+    }
+  }
+}
+      
+#Question 2 
+ 
+find_runs <- function(x, k) {
+  n <- length(x)
+  runs <- NULL
+
+  for (i in 1:(n - k + 1)) {
+    if (all(x[i:(i + k - 1)] == 1)) runs <- c(runs, i) else runs <- c(runs)
+  }
+  return(runs)
+}
+
+find_runs(c(1,0,0,1,1,0,1,1,1),2)
+
+#Question 3
+sort_vec <- function(x) {
+  if (length(x) < 2) return(x)
+  for (last in length(x):2) {
+    for (first in 1:(last - 1)) {
+      if (x[first] > x[first + 1]) {
+        temp <- x[first]
+        x[first] <- x[first + 1]
+        x[first + 1] <- temp
+      }
+    }
+  }
+  return(x)
+}
+
+sort_vec(c(3,1,2,4))
+
+
